a1b59a92a998d16d979408c8a3e4629f133baef6
«Anastasia <nastya-ryzhik-166@yandex.ru>
2022-10-02 13:05:03 +0300

    все задания выполнены

--- main.py
+++ main.py
@@ -3,64 +3,60 @@
 # - 6782 -> 23
 # - 0,56 -> 11
 
-# n = float(input('введите число '))
-# sum = 0
-# while n != 0:
-#     sum = sum + n %10
-#     n = n // 10
-# print(int(sum))
-    
+n = float(input('введите число: '))
+sum = 0
+while n != 0:
+    sum = sum + n %10
+    n = n // 10
+print(int(sum))
+
 # 2. Напишите программу, которая принимает на вход число N 
 # и выдает набор произведений чисел от 1 до N.
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
+# n = int(input('введите число: '))
 # prod = 1
-# n = int(input('введите число '))
-# list = []
+# array = []
 # for i in range(1, n + 1):
-#     list.append(prod * i)
+#     array.append(prod * i)
 #     prod = prod * i
-# print(list)
+# print(array)
 
 # 3. Задайте список из n чисел последовательности (1+1/n)^n 
 # и выведите на экран их сумму.
 
-# n = int(input('введите число '))
+# n = int(input('введите число: '))
 # list = []
 # sum = 0
-# j = 0
-# for i in range(1, n+1):
-#     sum += ((1+1/i)**i)
+# for i in range(1, n + 1):
+#     sum += ((1 + 1 / i) ** i)
 # print(round(sum, 2))
 
 # 4. Задайте список из N элементов, заполненных числами из промежутка [-N, N]. 
 # Найдите произведение элементов на указанных позициях. 
 # Позиции хранятся в файле file.txt в одной строке одно число.
 
-## решение записала с урока, сама не совсем поняла его
-
-# import random
-# n = int(input())
+# n = int(input('введите число N: '))
 # a = []
-# for i in range(n):
-#     a.append(random.randint(-n,n))
+# for i in range(-n, n + 1):
+#     a.append(i)
 # print(a)
-
-# m = open('file.txt', 'w')
-# len_m = random.randint(2,n)
-# for i in range(len_m):
-#     m.write(str(random.randint(0, n-1)))
-#     m.write('\n')
+#
+# # попыталась по логике лекции написать программу, но она не работает. Помогите разобраться
+# # ошибка здесь:  prod = prod * list[line] - unsupported operand type(s) for *: 'int' and 'types.GenericAlias'
+#
 # prod = 1
-# f = open('file.txt', 'r')
-# for i in f.readline():
-#     prod *= a[int(f.readline)]
+# data = open('file.txt', 'r')
+# for line in data:
+#     prod = prod * list[line]
+# data.close()
 # print(prod)
 
 # 5. Реализуйте алгоритм перемешивания списка.
+# не знаю, как написать алгоритм, нашла готовый метод shuffle
 
-from random import * # импортируем все функции из библиотеки
-list = [randint(-10, 10) for _ in range(randint(5, 10))]
-print(list)
-shuffle(list)
-print(list)
+# from random import *
+# array = [randint(0, 10) for _ in range(randint(0, 5))]
+# print(array)
+# shuffle(array)
+# print(array)
